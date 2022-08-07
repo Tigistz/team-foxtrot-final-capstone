c36022b83ed734871862669fed7d9546adf3cf84
Robert Snodgrass <rsnodgrass356@gmail.com>
2022-08-07 15:00:30 -0400

    Backend test 1

---
+++ java/src/main/java/com/techelevator/controller/BookController.java
@@ -0,0 +1,73 @@
+package com.techelevator.controller;
+
+//import com.techelevator.model.HomeNotFoundException;
+//import com.techelevator.model.Home;
+import com.techelevator.dao.BookDAO;
+import com.techelevator.model.Book;
+import com.techelevator.model.BookNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class BookController {
+
+    @Autowired
+    private BookDAO dao;
+
+
+    @RequestMapping(path="/hello", method= RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+
+    /**
+     * Return all books
+     *
+     * @return List<Book>  -  a list of all books in the user's reading list
+     */
+    @RequestMapping(path = "/mybooks", method = RequestMethod.GET)
+    public List<Book> retrieveAllBooks() {
+        return dao.retrieveAllBooks();
+    }
+
+
+    /**
+     * Add a new book to reading list, same page @/mybooks
+     *
+     * @return 201 Created if successful
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "/mybooks", method = RequestMethod.POST)
+    public Book addBook(@RequestBody Book book) {
+        return dao.addBook(book);
+    }
+
+
+
+//    @RequestMapping(path = "/mybooks/{bookId}", method = RequestMethod.GET)
+//    public Book retrieveBookByID(@PathVariable String bookId) throws BookNotFoundException {
+//        return null;
+//        //return dao.retrieveHomeByMLSId(mlsId);
+//    }
+
+
+    /**
+     * Deletes a book from the reading list, same page @/mybooks / ID
+     *
+     * @return 204 Created if successful
+     */
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/mybooks/{bookId}", method = RequestMethod.DELETE)
+    public void deleteBook(@PathVariable int bookId) throws BookNotFoundException {
+        dao.deleteBook(bookId);
+    }
+
+
+
+}
---
+++ java/src/main/java/com/techelevator/dao/JDBCBookDAO.java
@@ -0,0 +1,90 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Book;
+import com.techelevator.model.BookNotFoundException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JDBCBookDAO implements BookDAO{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JDBCBookDAO(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public List<Book> retrieveAllBooks() {
+        List<Book> books = new ArrayList<>();
+
+        String sql = "SELECT * " +
+                "FROM inventory ";
+                //"JOIN users ON users.userid = inventory.userid";
+                //WHERE users.userid = ?;
+
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);  //, principal.userId);
+
+        while(results.next()) {
+
+            Book book = mapRowToBook(results);
+            books.add(book);
+        }
+        return books;
+    }
+
+    @Override
+    public Book addBook(Book newBook){
+        String bookSQL = "INSERT INTO inventory (book_isbn, book_title, book_author, book_genre) " +  //TODO add the userID from principal
+                "VALUES(?,?,?,?) RETURNING book_id";
+        Integer id = jdbcTemplate.queryForObject(bookSQL, Integer.class,
+                newBook.getIsbn(),
+                newBook.getTitle(),
+                newBook.getAuthor(),
+                newBook.getGenre()
+                );
+        newBook.setBookId(id);
+        return newBook;
+
+    }
+
+    @Override
+    public void deleteBook(int bookToDeleteId) throws BookNotFoundException{  //website.com/mybooks/123
+        //retrieve where the Book ID in the database matches the ones we're trying to delete
+        String bookIdSQL = "SELECT book_id FROM inventory WHERE book_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(bookIdSQL, bookToDeleteId);
+
+        //check if the book at the ID we're trying to delete actually exists in the database
+
+        if(results.next()){
+            //if it exists we have the bookId to delete now, send another SQL query
+            String sql = "DELETE FROM inventory WHERE book_id = ?";
+            jdbcTemplate.update(sql, bookToDeleteId);
+        }
+        else{
+            throw new BookNotFoundException();
+        }
+
+    }
+
+
+
+    private Book mapRowToBook(SqlRowSet results) {
+        Book book = new Book();
+
+        book.setAuthor(results.getString("book_author"));
+        book.setTitle(results.getString("book_title"));
+        book.setBookId(results.getInt("book_id"));
+        book.setIsbn(results.getString("book_isbn"));
+        book.setGenre(results.getString("book_genre"));
+        book.setUserId(results.getInt("user_id"));
+
+        return book;
+    }
+}
