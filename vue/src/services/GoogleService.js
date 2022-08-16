de091a069aa6ed2f5027a97de92fcfb357369817
Tigist Yohannes <ytigist21@gmail.com>
2022-08-16 13:38:11 -0400

    capstone

--- vue/src/services/GoogleService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api_key = '&key=AIzaSyA2SB7helUW9bOBwnGTglWfkA31h0ovovg'
-
-export default {
-    getBookList(filter, query){
-        return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + filter + query + '&orderBy=relevance&maxResults=9' + api_key);
-    }
-}
